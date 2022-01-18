9f185b88803a6da5842369a007fc4734525ceab9
박찬영 <pcydeveloper@gmail.com>
2022-01-18 23:41:44 +0900

    chore: 빌드 된 페이지 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220118.2232';
+const cacheName = 'chirpy-20220118.2341';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
