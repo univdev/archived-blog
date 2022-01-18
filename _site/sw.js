d1eb76b02a6028f032f7aa3e4c5f642eaf168b24
박찬영 <pcydeveloper@gmail.com>
2022-01-18 23:24:28 +0900

    chore: .gitignore 제거

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220118.2221';
+const cacheName = 'chirpy-20220118.2224';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
